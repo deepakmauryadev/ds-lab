234f15fd92f123d8bbc51cbf8c6aeeff5f376ac4
Deepak Maurya <dee221403@gmail.com>
2024-11-07 12:41:49 +0530

    Changed code

--- lab08/01AssignmentB.c
+++ lab08/01AssignmentB.c
@@ -1,19 +1,29 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define SIZE 100
+
 typedef struct {
-  int front, rear, size, *arr;
+  int front, rear, size, arr[SIZE];
 } Queue;
 
-Queue* createQueue(int size) {
+Queue* createQueue() {
   Queue *q = (Queue *)malloc(sizeof(Queue));
   q->front = q->rear = -1;
-  q->size = size;
-  q->arr = (int *)malloc(size * sizeof(int));
+  q->size = SIZE;
   return q;
 }
 
-void enqueue(Queue *q, int data) {
+void enqueueFront(Queue *q, int data) {
+  if (q->rear >= q->size-1) printf("Queue Overflow!\n");
+  else if (q->rear == -1 && q->front == -1) {
+    q->front = q->rear = 0;
+    q->arr[q->front] = data;
+  } else if (q->front == 0) printf("No space in front!\n");
+  else q->arr[--(q->front)] = data;
+}
+
+void enqueueRear(Queue *q, int data) {
   if (q->rear >= q->size-1) printf("Queue Overflow!\n");
   else if (q->rear == -1 && q->front == -1) {
     q->front = q->rear = 0;
@@ -21,7 +31,7 @@ void enqueue(Queue *q, int data) {
   } else q->arr[++(q->rear)] = data;
 }
 
-int dequeueFront(Queue *q) {
+int dequeue(Queue *q) {
   int d;
   if (q->front == -1 && q->rear == -1) {
     d = -1;
@@ -30,19 +40,6 @@ int dequeueFront(Queue *q) {
     d = q->arr[q->rear];
     q->front = q->rear = -1;
   } else d = q->arr[(q->front)++];
-  
-  return d;
-}
-
-int dequeueRear(Queue *q) {
-  int d;
-  if (q->front == -1 && q->rear == -1) {
-    d = -1;
-    printf("Queue Underflow!\n");
-  } else if (q->front == q->rear) {
-    d = q->arr[q->rear];
-    q->front = q->rear = -1;
-  } else d = q->arr[(q->rear)--];
   
   return d;
 }
@@ -61,16 +58,12 @@ void displayQueue(Queue *q) {
 }
 
 int main() {
-  int n;
-  printf("Enter the size of queue: ");
-  scanf("%d", &n);
-
-  Queue *q = createQueue(n);
+  Queue *q = createQueue();
 
   printf("\t\tMENU\n");
+  printf("2. Insert at left\n");
   printf("1. Insert at right\n");
-  printf("2. Delete from left\n");
-  printf("3. Delete from right.\n");
+  printf("3. Delete from left\n");
   printf("4. Display\n");
   printf("5. Exit\n");
 
@@ -85,13 +78,15 @@ int main() {
       case 1: 
         printf("Enter element: ");
         scanf("%d", &elem);
-        enqueue(q, elem);
+        enqueueFront(q, elem);
         break;
       case 2:
-        printf("%d deleted!\n", dequeueFront(q));
+        printf("Enter element: ");
+        scanf("%d", &elem);
+        enqueueRear(q, elem);
         break;
       case 3:
-        printf("%d deleted!\n", dequeueRear(q));
+        printf("%d deleted!\n", dequeue(q));
         break;
       case 4:
         displayQueue(q);
