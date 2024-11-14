3b3361bdda2d13834c1c88496e063e13ded9a920
Deepak Maurya <dee221403@gmail.com>
2024-11-14 12:24:22 +0530

    Initial Commit

---
+++ lab11/03Assignment.c
@@ -0,0 +1,54 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+int partision(int arr[], int start, int end) {
+  int pivot = arr[start];
+  int i=start, j=end;
+
+  while (i < j) {
+    while (arr[i] <= pivot && i <= end-1) i++;
+    while (arr[j] > pivot && j >= start+1) j--;
+
+    if (i < j) swap(&arr[i], &arr[j]);
+  }
+  swap(&arr[start], &arr[j]);
+  return j;
+}
+
+void quickSort(int arr[], int n, int start, int end) {
+  if (start < end) {
+    int p = partision(arr, start, end);
+
+    quickSort(arr, n, start, p-1);
+    quickSort(arr, n, p+1, end);
+  }
+}
+
+int main() {
+  int n;
+  printf("Enter size of array: ");
+  scanf("%d", &n);
+
+  int arr[n];
+
+  int x;
+  for (int i=0; i<n; i++) {
+    printf("%d: ", i);
+    scanf("%d", &x);
+    arr[i] = x;
+  }
+
+  quickSort(arr, n, 0, n-1);
+  printf("Ascending Order: ");
+  for (int i=0; i<n; i++) 
+    printf("%d ", arr[i]);
+  printf("\n");
+
+  return 0;
+}
